8b1feb9b9874166c735ed9715240a88f25c6a7e8
xnorain001 <3385842328@qq.com>
2022-07-15 00:22:53 +0800

    Update: refactor

--- src/components/GachaAnalysis.vue
+++ src/components/GachaAnalysis.vue
@@ -38,11 +38,19 @@
         <template v-slot:top-right>
           <q-select
             filled
+            v-model="selectedTimeMode"
+            :options="timeModeOptions"
+            class="mr-5"
+            style="min-width: 150px;"
+            label="时间"
+          />
+          <q-select
+            filled
             v-model="selectedRankType"
             :options="rankTypeOptions"
             class="mr-5"
             style="min-width: 150px;"
-            label="选择星级"
+            label="星级"
           />
           <q-input
             filled
@@ -123,10 +131,6 @@ export default {
     Chart
   },
 
-  mounted () {
-    
-  },
-
   data () {
     return {
       logFile: null, // log.txt
@@ -170,12 +174,13 @@ export default {
         '常驻祈愿': '200'
       },
       fiveStarItems: [],
+      formattedFiveStartItem: [],
       fiveStarItemsOfResident: [
         '迪卢克', '刻晴', '琴', '七七', '莫娜', '阿莫斯之弓', '天空之翼', 
         '四风原典', '天空之卷', '和璞鸢','天空之脊', '狼的末路', '天空之傲', 
         '风鹰剑', '天空之刃'
       ],
-      averageNumberOfFiveStartItems: null,
+      averageCost: null,
       selectedRankType: '四星和五星',
       rankTypeOptions: ['四星和五星', '四星', '五星'],
       rankTypeMap: {
@@ -218,18 +223,15 @@ export default {
           },
           barWidth: 40,
         }
-      }
+      },
+      selectedTimeMode: '由近至远',
+      timeModeOptions: ['由近至远', '由远至近']
     }
   },
 
   methods: {
     clearKeyword () {
       this.keyword = ''
-    },
-
-    async getUsername () {
-      const data = await request.get('/getUsername')
-      console.log(data)
     },
 
     // 选取文件后被拒绝的回调
@@ -242,7 +244,7 @@ export default {
       this.logFile = files[0]
     },
 
-    // 开始抽卡分析
+    // 上传文件获取必须参数
     async startUploadFile () {
 
       // 确保存在 log.txt
@@ -251,8 +253,8 @@ export default {
         return
       }
 
-      // 清除之前的抽卡记录
-      this.resetGachaData()
+      // 清除之前的相关数据
+      this.resetData()
 
       const formData = new FormData
 
@@ -269,26 +271,39 @@ export default {
         } = await request.post('/uploadLogFile', formData)
 
         if (status === 200) {
-          suc('正在获取抽卡记录')
-
-          // 更新卡池类型
-          // params.gacha_type = this.gachaType
-
-          // // 获取抽卡记录
-          // const { data: gachaRecords } = await request.get('/getAllGachaRecords', {
-          //     params
-          //   }
-          // )
-
-          // this.gachaRecords = gachaRecords
-
-          const gachaRecords = JSON.parse(localStorage.getItem('gachaRecords'))
-          this.gachaRecords = gachaRecords
+          this.getGachaRecords(/* params */)
         } else {
           throw new Error(message)
         }
       } catch (error) {
         fail(error.message)
+      }
+    },
+
+    // 获取抽卡记录
+    getGachaRecords (/* params */) {
+      try {
+        const closeNotify = suc('正在获取抽卡记录')
+
+        // 用户选择的卡池类型
+        // params.gacha_type = this.gachaType
+
+        // // 获取抽卡记录
+        // const { data: gachaRecords } = await request.get('/getAllGachaRecords', {
+        //     params
+        //   }
+        // )
+
+        // 保存抽卡记录
+        // this.gachaRecords = gachaRecords
+
+        const gachaRecords = JSON.parse(localStorage.getItem('gachaRecords'))
+        this.gachaRecords = gachaRecords
+
+        closeNotify()
+        suc('成功获取抽卡记录')
+      } catch (error) {
+        fail(error)
       }
     },
 
@@ -300,131 +315,30 @@ export default {
       clearBtn.click()
     },
 
-    // 绘制饼图
+    // 绘制柱状图
     drawBarChart () {
-      const names = this.fiveStarItems.map(item => {
+      console.log(this.formattedFiveStartItem)
+      const names = this.formattedFiveStartItem.map(item => {
         return item.name
       })
-      const count = this.fiveStarItems.map(item => {
-        return item.counter
+      const count = this.formattedFiveStartItem.map(item => {
+        return item.cost
       })
       this.barChartOptions.xAxis.data = names
       this.barChartOptions.series.data = count
     },
 
     // 生成表格数据
-    createGachaTableRows (selectedRankType) {
-      const { gachaRecords } = this
-
-      if (selectedRankType === '四星') {
-        for (let i = gachaRecords.length - 1; i >= 0; i--) {
-
-          const record = gachaRecords[i]
-
-          // 去除三星武器
-          if (record.rank_type === '3' || record.rank_type === '5') {
-            continue
-          }
-
-          const {
-            item_type: itemType, 
-            name, 
-            rank_type: rankType,
-            time 
-          } = record
-
-          const gachaType = this.selectedMode
-
-          const formattedRecord = {
-            itemType,
-            name,
-            gachaType,
-            rankType,
-            time
-          }
-          this.gachaTable.rows.unshift(formattedRecord)
-        }
-      }
-      else if (selectedRankType === '五星') {
-        for (let i = gachaRecords.length - 1; i >= 0; i--) {
-
-          const record = gachaRecords[i]
-
-          // 去除三星武器
-          if (record.rank_type === '3' || record.rank_type === '4') {
-            continue
-          }
-
-          const {
-            item_type: itemType, 
-            name, 
-            rank_type: rankType,
-            time 
-          } = record
-
-          const gachaType = this.selectedMode
-
-          const formattedRecord = {
-            itemType,
-            name,
-            gachaType,
-            rankType,
-            time,
-          }
-          this.gachaTable.rows.unshift(formattedRecord)
-        }
-      }
-      else {
-        for (let i = gachaRecords.length - 1; i >= 0; i--) {
-
-          const record = gachaRecords[i]
-
-          // 去除三星武器
-          if (record.rank_type === '3') {
-            continue
-          }
-
-          const {
-            item_type: itemType, 
-            name, 
-            rank_type: rankType,
-            time 
-          } = record
-
-          const gachaType = this.selectedMode
-
-          const formattedRecord = {
-            itemType,
-            name,
-            gachaType,
-            rankType,
-            time
-          }
-
-          this.gachaTable.rows.unshift(formattedRecord)
-        }
-      }
-
-      // 拷贝一份，用于后续搜索
-      this.gachaRecords.sourceRows = this.gachaTable.rows
-    },
-
-    // 根据星级过滤记录
-    filterRecords () {
-      // ...
-    },
-    
-    // 计算五星道具平均所用抽数
-    computeAverageNumberOfFiveStartItemsAndMissingRate () {
-      let counter = 0
+    createGachaTableRows () {
       const { gachaRecords } = this
 
       for (let i = gachaRecords.length - 1; i >= 0; i--) {
-        counter++
+        const record = gachaRecords[i]
 
-        const record =gachaRecords[i]
-
-        const gachaType = this.selectedMode
+        // 去除三星武器
+        if (record.rank_type === '3') {
+          continue
+        }
 
         const {
           item_type: itemType, 
@@ -432,7 +346,7 @@ export default {
           rank_type: rankType,
           time 
         } = record
-
+        const gachaType = this.selectedMode
         const formattedRecord = {
           itemType,
           name,
@@ -441,23 +355,166 @@ export default {
           time
         }
 
-        // 记录出现的五星物品和所用抽数
-        if (rankType === '5') {
-          const isResident = this.fiveStarItemsOfResident.includes(name)
-          formattedRecord.isResident = isResident
-          formattedRecord.counter = counter
-          this.fiveStarItems.unshift(formattedRecord)
-          counter = 0
-        }
+        this.gachaTable.rows.unshift(formattedRecord)
       }
 
-      const formattedFiveStartItem = this.fiveStarItems.slice()
-      formattedFiveStartItem.length--
+      // 拷贝一份，用于后续搜索
+      this.gachaRecords.sourceRows = this.gachaTable.rows
 
+      // if (selectedRankType === '四星') {
+      //   for (let i = gachaRecords.length - 1; i >= 0; i--) {
+
+      //     const record = gachaRecords[i]
+
+      //     // 去除三星武器
+      //     if (record.rank_type === '3' || record.rank_type === '5') {
+      //       continue
+      //     }
+
+      //     const {
+      //       item_type: itemType, 
+      //       name, 
+      //       rank_type: rankType,
+      //       time 
+      //     } = record
+
+      //     const gachaType = this.selectedMode
+
+      //     const formattedRecord = {
+      //       itemType,
+      //       name,
+      //       gachaType,
+      //       rankType,
+      //       time
+      //     }
+      //     this.gachaTable.rows.unshift(formattedRecord)
+      //   }
+      // }
+      // else if (selectedRankType === '五星') {
+      //   for (let i = gachaRecords.length - 1; i >= 0; i--) {
+
+      //     const record = gachaRecords[i]
+
+      //     // 去除三星武器
+      //     if (record.rank_type === '3' || record.rank_type === '4') {
+      //       continue
+      //     }
+
+      //     const {
+      //       item_type: itemType, 
+      //       name, 
+      //       rank_type: rankType,
+      //       time 
+      //     } = record
+
+      //     const gachaType = this.selectedMode
+
+      //     const formattedRecord = {
+      //       itemType,
+      //       name,
+      //       gachaType,
+      //       rankType,
+      //       time,
+      //     }
+      //     this.gachaTable.rows.unshift(formattedRecord)
+      //   }
+      // }
+      // else {
+      //   for (let i = gachaRecords.length - 1; i >= 0; i--) {
+
+      //     const record = gachaRecords[i]
+
+      //     // 去除三星武器
+      //     if (record.rank_type === '3') {
+      //       continue
+      //     }
+
+      //     const {
+      //       item_type: itemType, 
+      //       name, 
+      //       rank_type: rankType,
+      //       time 
+      //     } = record
+
+      //     const gachaType = this.selectedMode
+
+      //     const formattedRecord = {
+      //       itemType,
+      //       name,
+      //       gachaType,
+      //       rankType,
+      //       time
+      //     }
+
+      //     this.gachaTable.rows.unshift(formattedRecord)
+      //   }
+      // }
+    },
+
+    // 根据星级过滤记录
+    filterRecords () {
+      // ...
+    },
+
+    // 获取五星道具所用抽数
+    getFiveStartItems () {
+      let count = 0
+      const { gachaRecords } = this
+
+      for (let i = gachaRecords.length - 1; i >= 0; i--) {
+        count++
+        
+        const record = gachaRecords[i]
+        const {
+          item_type: itemType,
+          name, 
+          rank_type: rankType,
+          time 
+        } = record
+        const fiveStartItem = {
+          itemType,
+          name,
+          rankType,
+          time
+        }
+
+        if (rankType === '5') {
+
+          // 判断是否是常驻
+          fiveStartItem.isResident = this.fiveStarItemsOfResident.includes(name)
+          fiveStartItem.cost = count
+          this.fiveStarItems.unshift(fiveStartItem)
+          count = 0
+        }
+      }
+    },
+
+    // 可能需要去除最后一金
+    getFormattedFiveStartItems () {
+      let { fiveStarItems } = this
+
+      // 除非最后一金用了 90 抽，否则去除掉，保证结果准确性。
+      if (fiveStarItems[fiveStarItems.length - 1].count === 90) {
+        this.formattedFiveStartItem = fiveStarItems.slice()
+      } else {
+        this.formattedFiveStartItem = fiveStarItems.slice(0, -1)
+      }
+    },
+    
+    // 计算五星道具平均所用抽数
+    computeAverageCost () {
+      const { formattedFiveStartItem } = this
       const total = formattedFiveStartItem.reduce((acc, cur) => {
-        return acc + cur.counter
+        return acc + cur.cost
       }, 0)
-      this.averageNumberOfFiveStartItems = total / formattedFiveStartItem.length
+
+      this.averageCost = total / formattedFiveStartItem.length
+      
+    },
+
+    // 计算歪率
+    computeMissingRate () {
+      const { formattedFiveStartItem } = this
       const missingCount = formattedFiveStartItem.reduce((acc, cur) => {
         return acc + (cur.isResident ? 1 : 0)
       }, 0)
@@ -465,9 +522,10 @@ export default {
       this.missingRate = missingCount / formattedFiveStartItem.length
     },
 
-    // 重置抽奖记录
-    resetGachaData () {
+    // 重置有关数据
+    resetData () {
       this.gachaRecords = []
+      this.gachaTable.sourceRows = []
       this.gachaTable.rows = []
       this.fiveStarItems = []
       this.averageNumberOfFiveStartItems = null
@@ -507,13 +565,22 @@ export default {
       }
 
       // 生成表格数据
-      this.createGachaTableRows(this.selectedRankType)
+      this.createGachaTableRows()
 
-      // 计算五星物品平均抽数和歪率
-      this.computeAverageNumberOfFiveStartItemsAndMissingRate()
+      // 获取五星道具
+      this.getFiveStartItems()
+
+      // 可能需要去除最后一金
+      this.getFormattedFiveStartItems()
 
       // 绘制柱状图
       this.drawBarChart()
+
+      // 计算五星道具平均抽数
+      this.computeAverageCost()
+
+      // 计算歪率
+      this.computeMissingRate()
 
       // 清除 log.txt
       this.clearFile()
